637b227f18f9d4284bba75f5dd9e7dc8208204f4
Thales <thaleesresende@gmail.com>
2025-04-17 19:21:17 -0300

    Criação da Classe D com métodos MD1, MD2

---
+++ D.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class D {
+private:
+    
+int D1;
+float D2;
+
+public: // getters / setters
+    
+    int getD1() { 
+        return D1; 
+    }
+    
+    void setD1(int valor) { 
+        D1 = valor; 
+    }
+
+    float getD2() { 
+        return D2; 
+    }
+    
+    void setD2(float valor) {
+         D2 = valor; 
+    }
+
+    // Métodos
+    void MD1() { 
+        cout << "Método MD1" << endl; 
+    }
+    
+    void MD2() { 
+        cout << "Método MD2" << endl; 
+    }
+};
